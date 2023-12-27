88f62a4d29bc955cb98f4586223833ea30980b50
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-26 18:36:35 -0800

    add line after conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Form, Image, Table } from "react-bootstrap";
-// import logoImageUrl from "./assets/logo-128.png";
+import logoImageUrl from "./assets/logo-128.png";
 import densities from "./densities.json";
 const ingredients = densities.map((d) => d.name);
 type TRecipeLine = {
@@ -26,7 +26,7 @@ const gramsPerOunce = 28;
 
 function App() {
   // https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/
-  const [recipeLines, setRecipeLines] = React.useState<TRecipeLine[]>([
+  const chocolateChipCookiesRecipe = [
     { amount: 1, unit: "c", ingredient: "butter" },
     { amount: 1, unit: "c", ingredient: "sugar" },
     { amount: 1, unit: "c", ingredient: "sugar, brown" },
@@ -38,16 +38,21 @@ function App() {
     { amount: 3, unit: "c", ingredient: "flour" },
     { amount: 2, unit: "c", ingredient: "chocolate chips" },
     { amount: 1, unit: "c", ingredient: "walnuts, chopped" },
-  ]);
+  ];
+  const [recipeLines, setRecipeLines] = React.useState<TRecipeLine[]>(
+    chocolateChipCookiesRecipe
+  ); // []
+  const addRecipeLine = (line: TRecipeLine) => {
+    setRecipeLines([line, ...recipeLines]);
+  };
   return (
     <Container>
-      {/* <Image src={logoImageUrl} /> */}
       <datalist id="ingredients">
         {ingredients.map((i, idx) => (
           <option value={i} key={idx} />
         ))}
       </datalist>
-      <Table striped variant="dark">
+      <Table striped size="sm">
         <thead>
           <tr>
             <th style={{ width: "10%" }}>Amount</th>
@@ -59,27 +64,31 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <RecipeLine />
+          <RecipeLine isActive={true} onLineAdded={addRecipeLine} />
           {recipeLines.map((line, idx) => (
-            <RecipeLine line={line} key={idx} />
+            <RecipeLine line={line} key={idx} isActive={false} />
           ))}
         </tbody>
       </Table>
+      <Image src={logoImageUrl} />
     </Container>
   );
 }
 
 type TRecipeLineProps = {
   line?: TRecipeLine;
+  isActive?: boolean;
+  onLineAdded?: (line: TRecipeLine) => void;
 };
 function RecipeLine(props: TRecipeLineProps) {
-  const { line } = props;
+  const { line, isActive } = props;
   const [amount, setAmount] = React.useState(line?.amount || undefined);
   const [unit, setUnit] = React.useState(line?.unit || "t");
   const [ingredient, setIngredient] = React.useState(line?.ingredient || "");
-  const [grams, setGrams] = React.useState("");
+  const [grams, setGrams] = React.useState(<></>);
   const [pounds, setPounds] = React.useState("");
   const [ounces, setOunces] = React.useState("");
+  const [lineIsActive, setLineIsActive] = React.useState(isActive);
 
   React.useEffect(() => {
     if (amount && unit && ingredient) {
@@ -94,29 +103,44 @@ function RecipeLine(props: TRecipeLineProps) {
             ? amount * density.g_whole
             : amount * density.g_ml * unitToGramsMap.get(unit)!;
         } else {
-          // no density found, assume 1 g/ml
-          grams = amount * unitToGramsMap.get(unit)!;
+          // no matching ingredient found, do nothing
+          return;
         }
       }
       if (grams >= 1000) {
-        setGrams((grams / 1000).toFixed(2) + " kg");
+        setGrams(
+          <>
+            {(grams / 1000).toFixed(2)}
+            <small>kg</small>
+          </>
+        );
       } else {
-        setGrams(grams.toFixed(2) + " g");
+        setGrams(
+          <>
+            {grams.toFixed(2)}
+            <small>g</small>
+          </>
+        );
       }
 
       const pounds = grams / gramsPerPound;
       const ounces = grams / gramsPerOunce;
       setPounds(pounds.toFixed(2) + " lb");
       setOunces(ounces.toFixed(2) + " oz");
+
+      if (lineIsActive && props.onLineAdded) {
+        props.onLineAdded({ amount, unit, ingredient });
+        setLineIsActive(false);
+      }
     } else {
-      setGrams("");
+      setGrams(<></>);
       setPounds("");
       setOunces("");
     }
   }, [amount, unit, ingredient]);
 
   return (
-    <tr>
+    <tr className={isActive ? "table-active table-group-divider" : ""}>
       <td>
         <Form.Control
           type="number"
@@ -166,9 +190,9 @@ function RecipeLine(props: TRecipeLineProps) {
           size="lg"
         />
       </td>
-      <td className="fs-2 text-primary">{grams}</td>
-      <td className="fs-3 text-secondary">{pounds}</td>
-      <td className="fs-3 text-secondary">{ounces}</td>
+      <td className="fs-2 text-success">{grams}</td>
+      <td className="fs-3 text-muted">{pounds}</td>
+      <td className="fs-3 text-muted">{ounces}</td>
     </tr>
   );
 }
