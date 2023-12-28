7216fc361ceda2dee88b429b8bcd4a9c1cf20e52
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-27 18:50:21 -0800

    extract recipes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import { Container, Table } from "react-bootstrap";
 import { TRecipeLine } from "./types";
-import densities from "./densities.json";
 import { RecipeLine } from "./components/RecipeLine";
 import { TopNav } from "./components/TopNav";
-
+import densities from "./densities.json";
 export const ingredients = densities.map((d) => d.name);
 
 function App() {
-  // https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/
-  // const chocolateChipCookiesRecipe = [
-  //   { amount: "1", unit: "c", ingredient: "butter" },
-  //   { amount: "1", unit: "c", ingredient: "sugar" },
-  //   { amount: "1", unit: "c", ingredient: "sugar, brown" },
-  //   { amount: "2", unit: "whole", ingredient: "egg" },
-  //   { amount: "2", unit: "t", ingredient: "vanilla extract" },
-  //   { amount: "1", unit: "t", ingredient: "baking soda" },
-  //   { amount: "2", unit: "t", ingredient: "water" },
-  //   { amount: "1/2", unit: "t", ingredient: "salt, table" },
-  //   { amount: "3", unit: "c", ingredient: "flour" },
-  //   { amount: "2", unit: "c", ingredient: "chocolate chips" },
-  //   { amount: "1", unit: "c", ingredient: "walnuts, chopped" },
-  // ].reverse();
   const [recipeLines, setRecipeLines] = React.useState<TRecipeLine[]>([]);
   const addRecipeLine = (newLine: TRecipeLine) => {
     setRecipeLines([newLine, ...recipeLines]);
---
+++ src/recipes.ts
@@ -0,0 +1,14 @@
+// https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/
+export const chocolateChipCookiesRecipe = [
+  { amount: "1", unit: "c", ingredient: "butter" },
+  { amount: "1", unit: "c", ingredient: "sugar" },
+  { amount: "1", unit: "c", ingredient: "sugar, brown" },
+  { amount: "2", unit: "whole", ingredient: "egg" },
+  { amount: "2", unit: "t", ingredient: "vanilla extract" },
+  { amount: "1", unit: "t", ingredient: "baking soda" },
+  { amount: "2", unit: "t", ingredient: "water" },
+  { amount: "1/2", unit: "t", ingredient: "salt, table" },
+  { amount: "3", unit: "c", ingredient: "flour" },
+  { amount: "2", unit: "c", ingredient: "chocolate chips" },
+  { amount: "1", unit: "c", ingredient: "walnuts, chopped" },
+];
